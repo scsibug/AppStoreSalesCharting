a6a56132b1271266fe9e688a53590a9d546b20b4
Greg Heartsfield <scsibug@imap.cc>
2010-09-09 20:40:44 -0500

    Example of some simple charts, using the old (pre 9/6/2010) format.

---
+++ old_format.r
@@ -0,0 +1,27 @@
+sales <- read.table("SampleDailySalesOldFmt.txt", header=TRUE, sep="\t")
+
+# Pie chart of currencies
+
+currencies <- table(sales["Customer.Currency"])
+#currframe <- as.vector(as.data.frame(currencies)[1])
+
+png("currency_piechart.png")
+pie(table(sales["Customer.Currency"]), main="Currency by Sales")
+dev.off()
+
+png("currency_barchart.png")
+plot(sales["Customer.Currency"], main="Currency by Sales")
+dev.off()
+
+png("sale_count_barchart.png")
+plot(sales["End.Date"], main="Sales by Day")
+dev.off()
+
+png("sale_count_linechart.png")
+plot(table(sales["End.Date"]),
+     main="Sales by Day",
+     xlab="Date",
+     ylab="Sales",
+     type="l")
+dev.off()
+
